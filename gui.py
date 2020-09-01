9bc122ad0d127c83a6af95b0f4e60edfe06f1aec
Marcin Piątek <mapiatek@student.agh.edu.pl>
2020-09-01 21:09:50 +0200

    Zarys kodu od obslugi gui

--- gui.py
+++ gui.py
@@ -1,2 +1,84 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+import tkinter as tk
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+
+        p1 = Page_main(self)
+        p2 = Page2(self)
+        p3 = Page3(self)
+
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        b1 = tk.Button(buttonframe, text="Page 1", command=p1.lift)
+        b2 = tk.Button(buttonframe, text="Page 2", command=p2.lift)
+        b3 = tk.Button(buttonframe, text="Page 3", command=p3.lift)
+
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+
+        p1.show()
+
+
+
+
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
+
+class Page_main(Page):
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        label = tk.Label(self, text="This is page main")
+        label.pack(side="top", fill="both", expand=True)
+
+
+class Page2(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 2")
+       label.pack(side="top", fill="both", expand=True)
+
+class Page3(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 3")
+       label.pack(side="top", fill="both", expand=True)
+
+
+# class MainButton(tk.Frame):
+#     def __init__(self, okno):
+#         super().__init__()
+#         self["height"] = 150
+#         self["width"] = 150
+#         self["relief"] = RAISED
+#         self["bd"] = 8
+#         self["bg"] = "red"
+
+def topmenu(root,main):
+    menu = tk.Menu(root)
+    menutop = tk.Menu(menu)
+    menu.add_cascade(label="Opcje", menu=menutop)
+    menutop.add_command(label="Powrot", )
+    menutop.add_command(label="Zapisz", )
+    menutop.add_command(label="Wyjdz", )
+    root.config(menu=menu)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("400x400")
+    root.title("Aplikacja do fiszek")
+    topmenu(root,main)
+    root.mainloop()

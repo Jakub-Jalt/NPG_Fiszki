4bf45e1b6b278f3c6aec8f5e069f62742da6730c
Mateusz Turek <mturek@student.agh.edu.pl>
2020-09-02 18:28:23 +0200

    modyfikacja pliku tryby, dodanie pliku statistics

--- tryby.py
+++ tryby.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from typing import Callable, Mapping, Optional, List
+import datetime
 import obsluga_bazy
 
 
 max_points: int = 15            #maksymalna liczba punktów do uzyskania w jednej turze gry
 
-def game(mode: int) -> int:
+def game(mode: int) -> None:     # mode: 0 - nic, 1 - ang_pol, 2 - pol_ang
     points: int = 0
 
-    for i in range(0, 15):
+    for i in range(0, max_points):
         word: List[str] = obsluga_bazy.all_base.get_random_pair()
-        if Forro(word) == True:        # Foo - nazwa tymczasowa
-            points += 1
-    return points
+        if mode == 1:
+            if Foo(word[0]) == word[1]:        # Foo - nazwa tymczasowa
+                points += 1
+        elif mode == 2:
+            if Foo(word[1]) == word[0]:        # Foo - nazwa tymczasowa
+                points += 1
+
+    f = open("statistics.txt", "a")
+    f.write(str(datetime.datetime.now()) + " - " + str(points) + " \n")           #zapisanie liczby uzyskanych punktów do pliku
+    return
 
 
+def Foo(word: str) -> str:
+    print(word)
+    return input("")
 
 
+game(2)

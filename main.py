1cba0122a6221e7c246dd63dbda976308692caf9
jisume <61950946+jisume@users.noreply.github.com>
2020-09-02 18:38:20 +0200

    Rozwinięcie kodu main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import gui
-import obsluga_bazy
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# import gui
+# import edit_mode as e_mo # moduł odpowiedzialny za edycję fiszek
+# import stat_mode as s_mo # moduł odpowiedzialny za obsługę statystyki
+# import tryby
+#
+# uruchomienie gui
+# gui.main()
+#
+# --------------------
+# użytkownik wprowadza swój nickname
+# następuje przekierowanie do menu z wyborem akcji
+# --------------------
 
-gui.main() #odpalenie gui
+# --------------------
+# *wybór akcji*
+# gui przekazuje, która akcja został wybrana:
+# statystyka, tryb edycji fiszek, tryb nauki
+# wylogowanie - powrót do strony głównej
+# zostaje uruchomiony odpowedni moduł
+# --------------------

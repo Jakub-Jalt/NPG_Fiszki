b1db164492380af8f88b3c185ff3d1269d344f56
Mateusz Turek <mturek@student.agh.edu.pl>
2020-09-11 00:11:02 +0200

    poprawa funkcji statystyk

--- mode_game.py
+++ mode_game.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from typing import Callable, Mapping, Optional, List
-import os
-import datetime
+from typing import List
 import database
 import mode_stat as m_s
 import config
@@ -29,12 +27,6 @@ def game() -> str:
         config.word = database.all_base.get_random_pair()
     f.close()
 
-
-
-
-
-
-                                      # typechoos = 1 nauka, 2 wpisywanie
     if config.langchoos == "polish":
         return config.word[0]
     elif config.langchoos == "english":
@@ -53,7 +45,6 @@ def check2(received_word: str) -> str:
 
         if received_word == config.word[1]:
             config.pol_in_session += 1
-            print("Y")
             f = open("users_words\\" + config.nick + ".txt", "a", encoding="utf-8")
             f.write(str(config.word) + " \n")
             f.close()
@@ -62,7 +53,6 @@ def check2(received_word: str) -> str:
 
         if received_word == config.word[0]:
             config.eng_in_session += 1
-            print("X")
             f = open("users_words\\" + config.nick + ".txt", "a", encoding="utf-8")
             f.write(str(config.word) + " \n")
             f.close()
@@ -70,13 +60,10 @@ def check2(received_word: str) -> str:
 
 
 def save_session():
-    print("save_session")
     if config.langchoos == "polish":
         m_s.after_session(shown_words = config.words_in_session, pol_correct = config.pol_in_session)
-        print("Z")
     elif config.langchoos == "english":
         m_s.after_session(shown_words = config.words_in_session, eng_correct = config.eng_in_session)
-        print("Ź")
     config.words_in_session = 0
     config.pol_in_session = 0
     config.eng_in_session = 0
@@ -84,61 +71,3 @@ def save_session():
 
 
 
-
-
-"""
-
-    points: int = 0
-
-    for i in range(0, max_points):                  #Funkcja losuje słowo i sprawdza czy nie powtarza się z niedawno użytym
-        word: List[str] =  obsluga_bazy.all_base.get_random_pair()
-        f = open("users_words\\" + nick + ".txt" , "r", encoding="utf-8")
-        while str(word) in f.readlines():
-            print("powtórzone słowo" + str(word))
-            word = obsluga_bazy.all_base.get_random_pair()
-        #f.close()
-
-
-        if mode == 1:
-            if Foo(word[0]) == word[1]:        # Foo - nazwa tymczasowa
-                points += 1
-                f = open("users_words\\" + nick + ".txt", "a", encoding="utf-8")
-                f.write(str(word) + " \n")
-                f.close()
-        elif mode == 2:
-            if Foo(word[1]) == word[0]:        # Foo - nazwa tymczasowa
-                points += 1
-                f = open("users_words\\" + nick + ".txt", "a", encoding="utf-8")
-                f.write(str(word) + " \n")
-                f.close()
-        elif mode == 3:
-            Foo2(word[0] + " - " + word[1])     # Foo2 - nazwa tymczasowa
-
-    f = open("statistics\\" + nick + ".txt", "a")
-    f.write(str(datetime.datetime.now()) + " - " + str(points) + " \n")           #zapisanie liczby uzyskanych punktów do pliku
-    f.close()
-#---------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-def Foo(word: str) -> str:                      #Funkcja tymczasowa do testowania kodu
-    print(word)
-    return input("")
-#--------------------------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-def words_statistics() -> None:                 #Funkcja sterująca statystykami
-    print(os.getcwd())
-    folders = os.listdir("users_words")
-    print(folders)
-    for i in folders:
-        print(i)
-        f = open("users_words\\" + i, 'r', encoding="utf-8")
-        lines: List[str] = f.readlines()
-        f.close()
-
-"""

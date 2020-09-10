6794e0af8fecf1bd8fe9adf5bb1c1f76612ec99a
jisume <61950946+jisume@users.noreply.github.com>
2020-09-10 16:09:43 +0200

    Poprawki

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ langchoos = 'null'  # wybor jezyka, polish, englis
 typechoos = 1  # 1 - nauka, 2 - wpisywanie, 3 - statysyki, 4 - dodawane fiszek
 nick = 'Mateusz'  # nazwa użytkownika
 page = 0  # numer strony
-positions = 30  # liczba pozycji w pliku z danymi statystycznymi
+positions = 10  # liczba pozycji w pliku z danymi statystycznymi
 word = []
